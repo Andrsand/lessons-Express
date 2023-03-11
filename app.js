9a1deb091d3f17d3816b9dc43b0494e57afe915d
andrsand <angorko@list.ru>
2023-03-11 20:58:50 +0300

    Sending status codes

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 
-app.use(function (request, response) {
-    response.sendFile(__dirname + "/index.html");
+app.use("/home/foo/bar", function (request, response) {
+    response.status(404).send(`Ресурс не найден`);
 });
 
 app.listen(3000);
