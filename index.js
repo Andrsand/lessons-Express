61db1311072caa6687ce922c40ba41cde1c9a355
andrsand <angorko@list.ru>
2023-03-11 17:30:48 +0300

    Routes are registered

--- index.js
+++ index.js
@@ -5,7 +5,16 @@ const app = express();               // создаем объект прилож
 
 app.get("/", function (request, response) {  // определяем обработчик для маршрута "/"
 
-    response.send("<h2>Привет Express!</h2>");       // отправляем ответ
+    response.send("<h1>Главная страница</h1>");       // отправляем ответ
+});
+
+app.get("/about", function (request, response) {
+
+    response.send("<h1>О сайте</h1>");
+});
+app.get("/contact", function (request, response) {
+
+    response.send("<h1>Контакты</h1>");
 });
 
 app.listen(3000); // начинаем прослушивать подключения на 3000 порту
