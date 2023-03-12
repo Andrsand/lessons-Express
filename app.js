becda9f01b1175cdb035d5fd4cdde157268d3f16
andrsand <angorko@list.ru>
2023-03-12 17:26:17 +0300

    Переадресация по оносительным путям

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 
-app.use("/index", function (request, response) {
-    response.redirect("https://metanit.com")
+app.use("/home", function (request, response) {
+    response.redirect("about")
+});
+app.use("/about", function (request, response) {
+    response.send("<h1>About</h1>");
 });
 
 app.listen(3000);
