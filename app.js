a6924ddb091c76b03b768cc59d8e7caf2c3a32e7
andrsand <angorko@list.ru>
2023-03-19 07:12:41 +0300

    Router example

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 const express = require("express");
-   
 const app = express();
-   
-// создаем парсер для данных application/x-www-form-urlencoded
-const urlencodedParser = express.urlencoded({extended: false});
-  
-app.get("/", function (request, response) {
-    response.sendFile(__dirname + "/index.html");
+ 
+// определяем Router
+const productRouter = express.Router();
+ 
+// определяем маршруты и их обработчики внутри роутера
+productRouter.use("/create", function(request, response){
+  response.send("Добавление товара");
 });
-app.post("/", urlencodedParser, function (request, response) {
-    if(!request.body) return response.sendStatus(400);
-    console.log(request.body);
-    response.send(`${request.body.userName} - ${request.body.userAge}`);
+productRouter.use("/:id", function(request, response){
+  response.send(`Товар ${request.params.id}`);
 });
-   
-app.listen(3000, ()=>console.log("Сервер запущен..."));
+productRouter.use("/", function(request, response){
+  response.send("Список товаров");
+});
+// сопотавляем роутер с конечной точкой "/products"
+app.use("/products", productRouter);
+ 
+app.use("/about", function (request, response) {
+  response.send("О сайте");
+});
+ 
+app.use("/", function (request, response) {
+  response.send("Главная страница");
+});
+app.listen(3000);
