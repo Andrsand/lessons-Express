691cae7a28f793e494b158f7aa4a0bc099a9b422
andrsand <angorko@list.ru>
2023-03-19 22:15:33 +0300

    Partial Views in Handlebars

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 const express = require("express");
-
+const hbs = require("hbs");
 const app = express();
+
 app.set("view engine", "hbs");
+hbs.registerPartials(__dirname + "/views/partials");
 
-app.use("/contact", function (_, response) {
+app.use("/contact", function (request, response) {
 
-  response.render("contact.hbs", {
+  response.render("contact", {
     title: "Мои контакты",
-    emailsVisible: false,
-    emails: ["gavgav@mycorp.com", "mioaw@mycorp.com"],
+    email: "gavgav@mycorp.com",
     phone: "+1234567890"
   });
 });
 
-app.use("/", function (_, response) {
+app.use("/", function (request, response) {
 
-  response.send("Главная страница");
+  response.render("home.hbs");
 });
 app.listen(3000);
