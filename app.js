dcb090f3ceea125ab2889224d05f7765e243920d
andrsand <angorko@list.ru>
2023-03-19 07:29:14 +0300

    JSON and AJAX example

--- app.js
+++ app.js
@@ -1,27 +1,19 @@
 const express = require("express");
+
 const app = express();
- 
-// определяем Router
-const productRouter = express.Router();
- 
-// определяем маршруты и их обработчики внутри роутера
-productRouter.use("/create", function(request, response){
-  response.send("Добавление товара");
+// создаем парсер для данных в формате json
+const jsonParser = express.json();
+
+app.post("/user", jsonParser, function (request, response) {
+  console.log(request.body);
+  if (!request.body) return response.sendStatus(400);
+
+  response.json(request.body); // отправляем пришедший ответ обратно
 });
-productRouter.use("/:id", function(request, response){
-  response.send(`Товар ${request.params.id}`);
+
+app.get("/", function (request, response) {
+
+  response.sendFile(__dirname + "/index.html");
 });
-productRouter.use("/", function(request, response){
-  response.send("Список товаров");
-});
-// сопотавляем роутер с конечной точкой "/products"
-app.use("/products", productRouter);
- 
-app.use("/about", function (request, response) {
-  response.send("О сайте");
-});
- 
-app.use("/", function (request, response) {
-  response.send("Главная страница");
-});
+
 app.listen(3000);
